1d077d34b8b2eae572943d9bed99fb01dcd17e61
hxl <seuxl1@163.com>
2016-05-19 08:17:42 +0800

    init

--- loki/sprite.lua
+++ loki/sprite.lua
@@ -10,9 +10,9 @@ local sprite = class()
 function sprite:ctor()
     self.drawable = nil
     self.x, self.y, self.w, self.h = 0, 0, 0, 0
+    self.r, self.g, self.b, self.a = 0xff, 0xff, 0xff, 0xff
     self.rot = 0
     self.sx, self.sy = 1, 1
-    self.opacity = 0xff
 end
 
 function sprite:ps(x, y, s)
@@ -29,7 +29,14 @@ function sprite:sr(sx, sy, r)
 end
 
 function sprite:setopacity(op)
-    self.opacity = op
+    self.a = op
+end
+
+function sprite:setcolor(r, g, b, a)
+    self.r = r or self.r
+    self.g = g or self.g
+    self.b = b or self.b
+    self.a = a or self.a
 end
 
 function sprite:aabb()
@@ -38,7 +45,7 @@ end
 
 function sprite:draw()
     local r, g, b, a = love.graphics.getColor()
-    love.graphics.setColor(0xff, 0xff, 0xff, self.opacity)
+    love.graphics.setColor(self.r, self.g, self.b, self.a)
     love.graphics.draw(self.drawable, self.x, self.y, self.rot, self.sx, self.sy)
     love.graphics.setColor(r,g,b,a)
 end
